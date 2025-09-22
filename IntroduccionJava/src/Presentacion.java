731e22c47364e0cbfffbf20610b1b6768b93bbda
Bjarkans <benjamin.nramirezs@gmail.com>
2025-09-22 15:04:16 -0300

    Introduccion muy basica a java

---
+++ IntroduccionJava/src/Presentacion.java
@@ -0,0 +1,7 @@
+public class Presentacion {
+    public static void main(String[] args) {
+        System.out.println("Nombre: Benjamín Ramírez");
+        System.out.println("Edad: 28");
+        System.out.println("Pais: Chile");
+    }
+}
